7a8087e7770758cff8db88ddc606606b0df22dce
nrikedel96 <nrikedel96@gmail.com>
2015-01-08 21:19:16 +0100

    Update CalculadoraAvanzada.java

--- src/calculadoraavanzada/CalculadoraAvanzada.java
+++ src/calculadoraavanzada/CalculadoraAvanzada.java
@@ -15,7 +15,7 @@ public class CalculadoraAvanzada {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("inicio");
     }
     
 }
