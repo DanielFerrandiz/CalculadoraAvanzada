bde17484f3f870a4b41c9462f9dd42013c8e65d2
nrikedel96 <nrikedel96@gmail.com>
2015-01-12 19:31:49 +0100

    Update CalculadoraAvanzada.java

--- src/calculadoraavanzada/CalculadoraAvanzada.java
+++ src/calculadoraavanzada/CalculadoraAvanzada.java
@@ -15,12 +15,29 @@ public class CalculadoraAvanzada {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner introducir = new Scanner(system.in);
+        int menu,a,b,resp;
+        menu=introducir.nextInt();
         
         switch (menu):
         case 1:
         case 2:
+            System.out.println("Resta");
+            System.out.print("Introduce el valor de A : ");
+            a=introducir.nextInt();
+            System.out.print("Introduce el valor de B : ");
+            b=introducir.nextInt();
+            resp=a-b;
+             System.out.print("resp");
         case 3:
         case 4:
+            System.out.println("Division");
+            System.out.print("Introduce el valor de A : ");
+            a=introducir.nextInt();
+            System.out.print("Introduce el valor de B : ");
+            b=introducir.nextInt();
+            resp=a/b;
+             System.out.print("resp");
         case 5:
         case 6:
             
